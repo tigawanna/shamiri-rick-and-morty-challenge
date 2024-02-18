c60131776a60d2a2e2ccba76f01037a2cc471336
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-02-18 22:19:10 +0300

    add virtualizer to episodes

--- src/routes/components/search/search_location/EpisodeLocations.tsx
+++ src/routes/components/search/search_location/EpisodeLocations.tsx
@@ -1,6 +1,8 @@
 import { ListPagination } from "@/components/shared/pagination/ReactresponsivePagination";
 import { SearchListLocationsQuery$data } from "../__generated__/SearchListLocationsQuery.graphql";
 import { OneItemCard } from "../../shared/OneItemCard";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import React from "react";
 
 type EpisodesResponse = SearchListLocationsQuery$data["episodes"];
 
@@ -9,10 +11,25 @@ interface EpisodeLocationsProps {
 }
 
 export function EpisodeLocations({ episodes }: EpisodeLocationsProps) {
-  if (!episodes) return null;
-    const episode_locations = episodes?.results
-        ?.flatMap((e) => e?.characters)
-        .flatMap((c) => c?.location) ?? [];
+  const parentRef = React.useRef(null);
+
+  // const episode_locations =
+  //   episodes?.results
+  //     ?.flatMap((e) => e?.characters)
+  //     .flatMap((c) => c?.location) ?? [];
+const episode_locations = React.useMemo(() => {
+  return (
+    episodes?.results
+      ?.flatMap((e) => e?.characters)
+      .flatMap((c) => c?.location) ?? []
+  );
+}, [episodes]);
+
+  const rowVirtualizer = useVirtualizer({
+    count: episode_locations?.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 35,
+  });
   if (!episode_locations || episode_locations?.length === 0) {
     return (
       <div className="w-full min-h-[60vh] h-full flex items-center justify-center">
@@ -24,22 +41,22 @@ export function EpisodeLocations({ episodes }: EpisodeLocationsProps) {
   }
   return (
     <div className="w-full h-full flex flex-col gap-3 ">
-      <ul className="flex flex-wrap gap-4 justify-center w-full">
-        {episode_locations?.map((item, idx) => {
-          if (!item) return null;
-          const key = `${item?.id}${item?.name}${idx}`;
-          return (
-            <OneItemCard
-              href={`/locations/${item?.id}`}
-              key={key}
-              id={item.id}
-              name={item?.name}
-            />
-          );
-        })}
-      </ul>
-
-
+      <div ref={parentRef} className="h-fit">
+        <ul className="flex flex-wrap gap-4 justify-center w-full ">
+          {/* Only the visible items in the virtualizer, manually positioned to be in view */}
+          {rowVirtualizer.getVirtualItems().map((virtualItem) => {
+            const one_episode = episode_locations[virtualItem.index];
+            return (
+              <OneItemCard
+                href={`/locations/${one_episode?.id}`}
+                key={virtualItem.key}
+                id={one_episode?.id}
+                name={one_episode?.name}
+              />
+            );
+          })}
+        </ul>
+      </div>
 
       <ListPagination
         query_key="sep"
