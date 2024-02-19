03c963a3496b9daa0bfba77c11818ee102fa5691
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-02-19 21:02:21 +0300

    add profile route

--- src/routes/profile/index.page.tsx
+++ src/routes/profile/index.page.tsx
@@ -1,10 +1,21 @@
+import { ShamiriUsersResponse } from "@/lib/pb/database";
 import { PageProps, usePageContext, useQuery } from "rakkasjs";
+import dayjs from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 export default function ProfilePage({}: PageProps) {
   const { locals } = usePageContext();
-  const query = useQuery("viewer",() => {
-    return locals.pb.authStore.model
+  const query = useQuery("viewer", () => {
+    return locals.pb.authStore?.model as { viewer: ShamiriUsersResponse };
   });
+
+  const profile = query.data.viewer;
   return (
-    <div className="w-full h-full  flex flex-col items-center justify-center"></div>
+    <div className="w-full h-full  flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">{profile?.username}</h1>
+      <h1 className="text-xl font-bold">{profile?.email}</h1>
+      <h1 className="text-xl font-bold">{profile?.verified}</h1>
+      <h1 className="text-xl font-bold">{dayjs(profile?.created).fromNow()}</h1>
+    </div>
   );
 }
